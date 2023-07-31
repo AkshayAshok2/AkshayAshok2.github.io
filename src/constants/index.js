8a46abc6cfd573a4cbb7b906ff0c55200cd733f7
Akshay Ashok <akshayashok@ufl.edu>
2023-07-30 21:57:11 -0500

    Deploy attempt

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,7 +46,7 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Machine Learning Researcher",
       icon: mobile,
     },
     {
@@ -54,7 +54,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Student Leader",
       icon: creator,
     },
   ];
